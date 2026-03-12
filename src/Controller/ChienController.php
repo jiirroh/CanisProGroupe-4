059bb055a07b39e0f5ca814845d86a3c10ef1cf7
Youssef Lahrour <130902370+LahrourYoussef@users.noreply.github.com>
2026-03-12 13:04:28 +0000

    index

--- src/Controller/ChienController.php
+++ src/Controller/ChienController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/chien')]
 final class ChienController extends AbstractController
 {
+    
     #[Route(name: 'app_chien_index', methods: ['GET'])]
     public function index(ChienRepository $chienRepository): Response
     {
