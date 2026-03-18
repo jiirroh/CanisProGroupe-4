2160a7b225a968a5790f5118e28c4e6e38e7c8c8
LahrourYoussef <130902370+LahrourYoussef@users.noreply.github.com>
2026-03-18 14:03:13 +0000

    fix: mysqld_safe au lieu de service mysql

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,17 +4,13 @@ set -e
 
 # Symfony CLI
 curl -sS https://get.symfony.com/cli/installer | bash
-echo 'export PATH="$HOME/.symfony5/bin:$PATH"' >> /root/.bashrc
-echo 'export PATH="$HOME/.symfony5/bin:$PATH"' >> /root/.profile
 ln -sf /root/.symfony5/bin/symfony /usr/local/bin/symfony
 
 # MySQL
 apt-get update && apt-get install -y default-mysql-server
-service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS canispro_bdd; CREATE USER IF NOT EXISTS 'canispro'@'localhost' IDENTIFIED BY 'Trasio2026.'; GRANT ALL ON canispro_bdd.* TO 'canispro'@'localhost';"
-
-# PHP extensions
-docker-php-ext-install pdo_mysql
+mysqld_safe --skip-grant-tables &
+sleep 3
+mysql -e "CREATE DATABASE IF NOT EXISTS canispro_bdd; CREATE USER IF NOT EXISTS 'canispro'@'localhost' IDENTIFIED BY 'Trasio2026.'; GRANT ALL ON canispro_bdd.* TO 'canispro'@'localhost'; FLUSH PRIVILEGES;"
 
 # Composer
 composer install --working-dir=/workspaces/CanisProGroupe-4
